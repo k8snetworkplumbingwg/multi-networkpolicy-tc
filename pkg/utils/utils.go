d02c10ff14a033839a04f1f71efbb84491262503
Adrian Chiris <adrianc@nvidia.com>
2022-07-10 15:58:25 +0300

    Add additional methods to utils pacakge
    Add some utility methods that will be used in
    later commits
    
    Signed-off-by: Adrian Chiris <adrianc@nvidia.com>

---
+++ pkg/utils/utils.go
@@ -0,0 +1,75 @@
+package utils
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"strings"
+
+	multiv1beta1 "github.com/k8snetworkplumbingwg/multi-networkpolicy/pkg/apis/k8s.cni.cncf.io/v1beta1"
+	v1 "k8s.io/api/core/v1"
+)
+
+// PolicyNetworkAnnotation is annotation for multiNetworkPolicy,
+// to specify which networks(i.e. net-attach-def) are the targets
+// of the policy
+const PolicyNetworkAnnotation = "k8s.v1.cni.cncf.io/policy-for"
+
+// CheckNodeNameIdentical checks both strings point a same node
+// it just checks hostname without domain
+func CheckNodeNameIdentical(s1, s2 string) bool {
+	return strings.Split(s1, ".")[0] == strings.Split(s2, ".")[0]
+}
+
+// IsMultiNetworkpolicyTarget checks if pod is in running phase and is not hostNetwork
+func IsMultiNetworkpolicyTarget(pod *v1.Pod) bool {
+	if pod.Status.Phase == v1.PodRunning && !pod.Spec.HostNetwork {
+		return true
+	}
+
+	return false
+}
+
+// NetworkListFromPolicy returns a list of networks which apply to the provided MultiNetworkPolicy
+func NetworkListFromPolicy(policy *multiv1beta1.MultiNetworkPolicy) []string {
+	policyNetworksAnnot, ok := policy.GetAnnotations()[PolicyNetworkAnnotation]
+	if !ok {
+		return []string{}
+	}
+	policyNetworksAnnot = strings.ReplaceAll(policyNetworksAnnot, " ", "")
+	policyNetworks := strings.Split(policyNetworksAnnot, ",")
+
+	for idx, policyNetName := range policyNetworks {
+		// fill namespace
+		if !strings.ContainsAny(policyNetName, "/") {
+			policyNetworks[idx] = fmt.Sprintf("%s/%s", policy.Namespace, policyNetName)
+		}
+	}
+	return policyNetworks
+}
+
+// IPsFromStrings receives a list of IPs in string format and returns a list of net.IP
+func IPsFromStrings(ips []string) []net.IP {
+	netIPs := make([]net.IP, 0, len(ips))
+	for _, ip := range ips {
+		netIPs = append(netIPs, net.ParseIP(ip))
+	}
+	return netIPs
+}
+
+// IsIPv4 returns true if ip is of type(length) IPV4
+func IsIPv4(ip net.IP) bool {
+	return len(ip) == net.IPv4len
+}
+
+// PathExists returns true if path exists in the system or false if it doesnt
+// in case of error, and error is returned
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	} else if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
