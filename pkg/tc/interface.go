462c73cad1c49dafc219eae8886600d3a050ccd9
Adrian Chiris <adrianc@nvidia.com>
2022-07-11 11:23:49 +0300

    Add TC interface and TC types
    Signed-off-by: Adrian Chiris <adrianc@nvidia.com>

---
+++ pkg/tc/interface.go
@@ -0,0 +1,30 @@
+package tc
+
+import (
+	tctypes "github.com/Mellanox/multi-networkpolicy-tc/pkg/tc/types"
+)
+
+// TC defines an interface to interact with Linux Traffic Control subsystem
+// an implementation should be associated with a specific network interface (netdev).
+type TC interface {
+	// QDiscAdd adds the specified Qdisc
+	QDiscAdd(qdisc tctypes.QDisc) error
+	// QDiscDel deletes the specified Qdisc
+	QDiscDel(qdisc tctypes.QDisc) error
+	// QDiscList lists QDiscs
+	QDiscList() ([]tctypes.QDisc, error)
+
+	// FilterAdd adds filter to qdisc
+	FilterAdd(qdisc tctypes.QDisc, filter tctypes.Filter) error
+	// FilterDel deletes filter identified by filterAttr from qdisc
+	FilterDel(qdisc tctypes.QDisc, filterAttr *tctypes.FilterAttrs) error
+	// FilterList lists Filters on qdisc
+	FilterList(qdisc tctypes.QDisc) ([]tctypes.Filter, error)
+
+	// ChainAdd adds chain to qdiscss
+	ChainAdd(qdisc tctypes.QDisc, chain tctypes.Chain) error
+	// ChainDel deletes chain from qdisc
+	ChainDel(qdisc tctypes.QDisc, chain tctypes.Chain) error
+	// ChainList lists chains on qdisc
+	ChainList(qdisc tctypes.QDisc) ([]tctypes.Chain, error)
+}
---
+++ pkg/tc/types/chain.go
@@ -0,0 +1,95 @@
+package types
+
+import (
+	"fmt"
+	"strconv"
+)
+
+const (
+	// ChainDefaultParent is the default parent of a chain which is the ingress qdisc
+	ChainDefaultParent = 0xfffffff1
+	// ChainDefaultChain is the default chain number
+	ChainDefaultChain = 0
+)
+
+// Chain is an interface which represents a TC chain
+type Chain interface {
+	CmdLineGenerator
+	// Attrs returns chain attributes
+	Attrs() *ChainAttrs
+}
+
+// ChainAttrs are the attributes of a Chain
+type ChainAttrs struct {
+	Parent *uint32
+	Chain  *uint16
+}
+
+// ChainImpl is a concrete implementation of Chain
+type ChainImpl struct {
+	ChainAttrs
+}
+
+// Attrs implements Chain interface
+func (c *ChainImpl) Attrs() *ChainAttrs {
+	return &c.ChainAttrs
+}
+
+// GenCmdLineArgs implements CmdLineGenerator interface
+func (c *ChainImpl) GenCmdLineArgs() []string {
+	args := []string{}
+
+	if c.Parent != nil {
+		parent := fmt.Sprintf("%x:%x", uint16(*c.Parent>>16), uint16(*c.Parent))
+		args = append(args, "parent", parent)
+	}
+
+	if c.Chain != nil {
+		args = append(args, "chain", strconv.FormatUint(uint64(*c.Chain), 10))
+	}
+	return args
+}
+
+func NewChainImpl(parent *uint32, chain *uint16) *ChainImpl {
+	return &ChainImpl{ChainAttrs{
+		Parent: parent,
+		Chain:  chain,
+	}}
+}
+
+// builder
+
+// NewChainBuilder creates a new ChainBuilder
+func NewChainBuilder() *ChainBuilder {
+	return &ChainBuilder{}
+}
+
+// ChainBuilder is a Chain builder
+type ChainBuilder struct {
+	chain ChainImpl
+}
+
+// WithParent adds Chain Parent to ChainBuilder
+func (cb *ChainBuilder) WithParent(parent uint32) *ChainBuilder {
+	cb.chain.Parent = &parent
+	return cb
+}
+
+// WithChain adds Chain chain number to ChainBuilder
+func (cb *ChainBuilder) WithChain(chain uint16) *ChainBuilder {
+	cb.chain.Chain = &chain
+	return cb
+}
+
+// Build builds and returns a new Chain instance
+// Note: calling Build() multiple times will not return a completely
+// new object on each call. that is, pointer/slice/map types will not be deep copied.
+// to create several objects, different builders should be used.
+func (cb *ChainBuilder) Build() *ChainImpl {
+	if cb.chain.Chain == nil {
+		defChain := uint16(ChainDefaultChain)
+		cb.chain.Chain = &defChain
+
+	}
+	return NewChainImpl(cb.chain.Parent, cb.chain.Chain)
+}
