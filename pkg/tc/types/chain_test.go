96b9a6d83b095a2c5ff547220e3bc2056a22d8a6
Adrian Chiris <adrianc@nvidia.com>
2022-08-09 19:58:24 +0300

    Add Chain type tests
    Signed-off-by: Adrian Chiris <adrianc@nvidia.com>

---
+++ pkg/tc/types/chain_test.go
@@ -0,0 +1,55 @@
+package types_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/Mellanox/multi-networkpolicy-tc/pkg/tc/types"
+)
+
+var _ = Describe("Chain tests", func() {
+	parent := uint32(0xfffffff1)
+	chain := uint16(0)
+
+	assertChain := func(q *types.ChainImpl) {
+		ExpectWithOffset(1, q).ToNot(BeNil())
+		ExpectWithOffset(1, *q.Parent).To(Equal(parent))
+		ExpectWithOffset(1, *q.Chain).To(Equal(chain))
+	}
+
+	Describe("Creational", func() {
+		Context("NewChainImpl", func() {
+			It("Creates a new ChainImpl", func() {
+				c := types.NewChainImpl(&parent, &chain)
+
+				assertChain(c)
+			})
+		})
+
+		Context("ChainBuilder", func() {
+			It("Builds Chain with correct attributes", func() {
+				c := types.NewChainBuilder().WithParent(parent).WithChain(chain).Build()
+
+				assertChain(c)
+			})
+		})
+	})
+
+	Describe("Chain Interface", func() {
+		c := types.NewChainBuilder().WithParent(parent).WithChain(chain).Build()
+
+		Context("Attrs()", func() {
+			It("returns expected attrs", func() {
+				Expect(*c.Attrs().Parent).To(Equal(parent))
+				Expect(*c.Attrs().Chain).To(Equal(chain))
+			})
+		})
+
+		Context("CmdLineGenerator", func() {
+			It("generates expected command line args", func() {
+				expectedArgs := []string{"parent", "ffff:fff1", "chain", "0"}
+				Expect(c.GenCmdLineArgs()).To(Equal(expectedArgs))
+			})
+		})
+	})
+})
